66b653e860ea6654c486a12d01ed716242d32ac6
ex7763 <eddie55020@gmail.com>
2018-07-15 13:37:43 +0800

    Auto hugo

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Clean emacs backup
+rm *~
+
+# add source
+git add -A
+git commit -m "Auto hugo"
+git push origin master
+
 # Build the project.
 hugo -t hugo-lithium-theme
 
